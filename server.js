35842488ea50f04ce5c3a596728e18a4f5ce7cbb
Ashish Singh Rawat <ashishsrawat98@gmail.com>
2022-05-18 23:43:29 +0530

    first coommit
    update login

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require("./src/Database/connection")
 
 //Getting the port to run the server on
-const PORT = process.eventNames.PORT || 3000
+const PORT = process.eventNames.PORT || 8000
 
 //creating an express app
 const app = express()
@@ -15,7 +15,28 @@ const app = express()
 //Parsing the body, to support Application/JSON
 app.use(json());
 
-app.options('*', cors())
+var allowedOrigins = ['http://localhost:3000' ];
+app.use(cors({
+    origin: function (origin, callback) {    // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        } return callback(null, true);
+    },
+    credentials: true
+}));
+
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain)
 
 //Setting up routes for LOGIN, LOGOUT, RESET_PASSWORD
 app.use("/users",userRoutes);
